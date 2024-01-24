e44069fee7dc720e5a9b1f59d0cf937ede44bda2
Sahil-2002 <sahila.khandekar@gmail.com>
2024-01-24 17:10:49 +0530

    first

---
+++ cal.java
@@ -0,0 +1,8 @@
+public class cal {
+    public static void main(String[] args) {
+        System.out.println("welcome to my calculator ");
+        int a= 10, b=20;
+       int  add= a+b;
+        System.out.println("addition is "+(add));
+    }
+}
